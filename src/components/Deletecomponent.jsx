3c4a868fa6996493d5afa7d06cc87a0c48d85477
Tobi Egunjobi <egunjobithobbie4@gmail.com>
2026-04-24 16:58:35 +0100

    Updated codes

--- src/components/Deletecomponent.jsx
+++ src/components/Deletecomponent.jsx
@@ -3,6 +3,7 @@ import './Deletecomponent.css'
 
 const Deletecomponent = ({onCancel}) => {
   return (
+    <div className='delete-modal-overlay'>
     <div className='delete-modal'>
       <h2>Confirm Deletion</h2>
       <p>Are you sure you want to delete invoice #XM9141? This action cannot be undone</p>
@@ -10,6 +11,7 @@ const Deletecomponent = ({onCancel}) => {
         <button onClick={onCancel} className='cancel-action-button'>Cancel</button>
         <button className='delete-button'>Delete</button>
       </div>
+    </div>
     </div>
   )
 }
